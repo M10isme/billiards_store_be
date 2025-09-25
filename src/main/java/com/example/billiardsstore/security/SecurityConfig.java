dbcb1c191816731cfba14f491d2a3f261e6c48d5
M10isme <80384574+M10isme@users.noreply.github.com>
2025-09-26 00:13:00 +0700

    Update CORS configuration to allow new frontend URL

--- src/main/java/com/example/billiardsstore/security/SecurityConfig.java
+++ src/main/java/com/example/billiardsstore/security/SecurityConfig.java
@@ -102,7 +102,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // FE
+        config.setAllowedOrigins(List.of("https://billiards-store-fe.vercel.app")); // FE
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
         config.setAllowCredentials(true);
